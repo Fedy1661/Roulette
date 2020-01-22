9f3499d0e488fe27a4d158a33484f5e6da9a269c
Fedor Konovalov <fedykonovalov@gmail.com>
2020-01-22 17:40:49 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/reset.css'
+import './styles/index.css';
+import App from './components/app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
