caaaf761475dcc0fd59aaa1230d23c00358bd875
Fedor Konovalov <fedykonovalov@gmail.com>
2020-01-23 16:59:20 +0300

    Updates

---
+++ precache-manifest.cbd75f97d9b07d5b570ddd15295e013e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195d1b31633464d7c827f8c53d6e9651",
+    "url": "/Roulette/index.html"
+  },
+  {
+    "revision": "10b38e49080e00aa3a1f",
+    "url": "/Roulette/static/css/main.b703202d.chunk.css"
+  },
+  {
+    "revision": "42f863515e40fa82726e",
+    "url": "/Roulette/static/js/2.1bc70978.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Roulette/static/js/2.1bc70978.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10b38e49080e00aa3a1f",
+    "url": "/Roulette/static/js/main.845269c6.chunk.js"
+  },
+  {
+    "revision": "bb15c4af8fcd5b3de5ba",
+    "url": "/Roulette/static/js/runtime-main.717b1951.js"
+  },
+  {
+    "revision": "a08672924fc99282e473d6bd984142ca",
+    "url": "/Roulette/static/media/pencils.a0867292.svg"
+  }
+]);
